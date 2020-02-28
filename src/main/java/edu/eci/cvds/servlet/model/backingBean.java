d3aa72cc6b4692a7f3b156aad3236869f4110445
PaoCuellar <paola.cuellar@mail.escuelaing.edu.co>
2020-02-28 18:29:31 -0500

    parte del 4

---
+++ src/main/java/edu/eci/cvds/servlet/model/backingBean.java
@@ -0,0 +1,68 @@
+package edu.eci.cvds.servlet.model;
+
+import javax.faces.bean.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+@ManagedBean(name = "guessBean")
+@ApplicationScoped
+public class backingBean {
+
+    private ArrayList<Integer> numIntentos;
+    private int premioAcumulado;
+    private int numeroRandom;
+    private String estado;
+
+    public backingBean(){
+        restart();
+    }
+
+    public void restart(){
+        Random r = new Random();
+        numeroRandom = r.nextInt(11);
+        numIntentos = new ArrayList<>();
+        premioAcumulado = 100000;
+        estado = "no gana";
+    }
+
+    public void guess(int num){
+        numIntentos.add(num);
+        if(num!=numeroRandom){
+            premioAcumulado-=10000;
+        }else{
+            estado = "gana";
+        }
+    }
+
+    public ArrayList<Integer> getIntentos() {
+        return numIntentos;
+    }
+
+    public void setIntentos(ArrayList<Integer> numIntentos) {
+        this.numIntentos = numIntentos;
+    }
+
+    public int getPremioAcumulado() {
+        return premioAcumulado;
+    }
+
+    public void setPremioAcumulado(int premioAcumulado) {
+        this.premioAcumulado = premioAcumulado;
+    }
+
+    public int getNumeroRandom() {
+        return numeroRandom;
+    }
+
+    public void setNumeroRandom(int numeroRandom) {
+        this.numeroRandom = numeroRandom;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
